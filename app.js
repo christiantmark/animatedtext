a12af86b03dad08a5240b53f9ba223b6be1fd9cc
christiantmark <christiantmark@gmail.com>
2021-12-28 12:55:18 -0800

    added logo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+
 const logo = document.querySelectorAll("#logo path");
 
-console.log('logo');
+console.info(logo);
 
 // for(let i = 0; i<logo.length; i++){
 //     console.log('Letter ${i} is ${logo[i].getTotalLength()}');
